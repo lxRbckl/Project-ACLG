da45efd047b74f91ec26e29a4d831d234770004d
lxRbckl <lxrbckl@gmail.com>
2024-05-29 20:16:41 -0500

    file structure

---
+++ app.py
@@ -0,0 +1,20 @@
+# import <
+from dash import Dash
+from dash_bootstrap_components import themes
+
+# >
+
+
+application = Dash(
+   
+   name = 'ACLG',
+   title = 'ACLG',
+   suppress_callback_exceptions = True,
+   external_stylesheets = [
+      
+      themes.GRID,
+      themes.BOOTSTRAP
+      
+   ]
+   
+)
