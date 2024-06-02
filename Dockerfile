dd7ab48f7f182d60fba2ae7f094d48d86caf8778
lxRbckl <lxrbckl@gmail.com>
2024-06-01 20:54:09 -0500

    Update

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,12 @@ FROM python:3.8-slim
 
 # referencing docker compose (find wiki) #
 ENV PORT ${PORT}
+ENV HOST ${HOST}
 
 WORKDIR /app
 COPY . .
 RUN pip install -r requirements.txt
-EXPOSE 8060
+EXPOSE ${PORT}
 
 # run dash application
 CMD ["python3", "main.py"]
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # import <
+from os import environ
 from source.frontend.layout import layout
 from source.frontend.callbacks import callbacks
 from source.configuration import (application, server)
@@ -7,6 +8,9 @@ from source.configuration import (application, server)
 
 
 if (__name__ == '__main__'):
+   
+   host = environ.get('HOST')
+   port = environ.get('PORT')
       
    objLayout = layout()
    objCallbacks = callbacks()
@@ -14,5 +18,5 @@ if (__name__ == '__main__'):
    objCallbacks.register()
    application.layout = objLayout.layout
    
-   application.run(host = '0.0.0.0', port = 8060)
+   application.run(host = host, port = port)
    
